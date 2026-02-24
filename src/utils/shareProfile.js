400a521cb7501a529b01dc8b2104c96308b74c3a
LaurenAricaG <laurendavid159@gmail.com>
2026-02-24 15:40:27 -0500

    Improve the text message when sharing GitHub profiles in the shareProfile function

--- src/utils/shareProfile.js
+++ src/utils/shareProfile.js
@@ -1,11 +1,11 @@
 export const shareProfile = async (user) => {
   const appUrl = `${window.location.origin}/?user=${user.login}`;
+  const message = `¡Echa un vistazo al perfil de GitHub de ${user.name || user.login}!\n${appUrl}`;
 
   if (navigator.share) {
     await navigator.share({
       title: `${user.login} | GitHub Profile`,
-      text: "Mira este perfil",
-      url: appUrl,
+      text: message,
     });
   } else {
     await navigator.clipboard.writeText(appUrl);
